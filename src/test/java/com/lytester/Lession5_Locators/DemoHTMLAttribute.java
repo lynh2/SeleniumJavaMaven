6b0b9cc403903a6ee4eb75442766f127b68267b5
HuongLy <nguyenly00048@gmail.com>
2025-08-31 22:39:24 +0700

    add lesson 5 locators

---
+++ src/test/java/com/lytester/Lession5_Locators/DemoHTMLAttribute.java
@@ -0,0 +1,7 @@
+package com.lytester.Lession5_Locators;
+
+public class DemoHTMLAttribute {
+    public static void main(String[] args) {
+        System.out.println("Demo HTML Attribute");
+    }
+}
